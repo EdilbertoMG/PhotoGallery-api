e9969e67122cf6897289e4bc5f20f11ed33e8f4c
edilbertomg <edilbertomarrugo03@hotmail.com>
2020-02-15 16:09:18 -0500

    Agregando headers y cors

--- src/app.js
+++ src/app.js
@@ -16,6 +16,16 @@ app.use(express.json());
 app.use(express.urlencoded({
     extended: false
 }));
+
+// Middlewares Configure headers and cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 const storage = multer.diskStorage({
     destination: path.join(__dirname, 'public/uploads'),
     limits: {
